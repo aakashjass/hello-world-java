7a459be37f229033adccc574848862ba183a1793
Aakash <48553495+aakashjass@users.noreply.github.com>
2021-12-21 13:36:03 +0530

    Add files via upload

---
+++ hello1.java
@@ -0,0 +1,15 @@
+class hello1
+{
+ public static void main(String... g)
+ {
+     System.out.println("hello universe");
+ }
+}
+
+class world
+{
+ public static void main(String... g)
+ {
+     System.out.println("hello multiverse");
+ }
+}
